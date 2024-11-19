171c9f08859ebf7c3eda5672c99c534e99c757fd
Mahjoubech <charkaouielmahjoub50@gmail.com>
2024-11-19 18:43:37 +0100

    add dom event and use array for click squar

--- main.js
+++ main.js
@@ -6,3 +6,16 @@ let data = [
     "3","4","5",
     "6","7","8"
 ]
+for(let itm of squr){
+    itm.addEventListener("click",()=>{
+       let value = itm.getAttribute("value") ;
+       let sqcnt = document.querySelector(`.squr[value='${value}']`)  
+       sqcnt.innerHTML = crr;
+       let index = value - 1;
+       data[index] = crr 
+      console.log(data);
+        winner();
+       crr === "X"? crr = "O": crr = "X";
+       
+      
+    })
